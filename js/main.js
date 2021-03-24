a431bee7982fde507520b834d4a5e0fed736f8d2
maxbendas <maxbendas86@gmail.com>
2021-03-24 16:10:18 +0200

    Second day

--- js/main.js
+++ js/main.js
@@ -38,10 +38,10 @@ modalCart.addEventListener('click', (e) => {
 {
     const scrollLinks = document.querySelectorAll('a.scroll-link')
 
-    for (let i = 0; i < scrollLinks.length; i++) {
-        scrollLinks[i].addEventListener('click', (e) => {
+    for (const scrollLink of scrollLinks) {
+        scrollLink.addEventListener('click', (e) => {
             e.preventDefault()
-            const id = scrollLinks[i].getAttribute('href')
+            const id = scrollLink.getAttribute('href')
             document.querySelector(id).scrollIntoView({
                 behavior: "smooth",
                 block: "start"
@@ -49,3 +49,69 @@ modalCart.addEventListener('click', (e) => {
         })
     }
 }
+
+// goods
+
+const more = document.querySelector('.more')
+const navigationLinks = document.querySelectorAll('.navigation-link')
+const longGoodsList = document.querySelector('.long-goods-list')
+
+const getGoods = async () => {
+    const result = await fetch('db/db.json')
+    if (!result.ok) {
+        throw 'Ошибочка вышла ' + result.status
+    }
+    return await result.json()
+}
+
+const createCard = ({label, img, name, description, id, price}) => {
+    const card = document.createElement('div')
+    card.className = 'col-lg-3 col-sm-6'
+    card.innerHTML = `
+    <div class="goods-card">
+        ${label ? `<span class="label">${label}</span>` : ''}        
+        <img src="db/${img}" alt="${name}" class="goods-image">
+        <h3 class="goods-title">${name}</h3>
+        <p class="goods-description">${description}</p>
+        <button class="button goods-card-btn add-to-cart" data-id="${id}">
+            <span class="button-price">$${price}</span>
+        </button>
+    </div>
+    `
+    return card
+}
+
+// renderCards()
+
+const renderCards = (data) => {
+    longGoodsList.textContent = ''
+    const cards = data.map(createCard)
+    longGoodsList.append(...cards)
+    document.body.classList.add('show-goods')
+}
+
+more.addEventListener('click', (e) => {
+    e.preventDefault()
+    getGoods().then(renderCards)
+})
+
+const filterCards = (field, value) => {
+    getGoods()
+        .then((data) => {
+            const filteredGoods = data.filter((good) => {
+                return good[field] === value
+            })
+            return filteredGoods
+        })
+        .then(renderCards)
+}
+
+navigationLinks.forEach((link)=>{
+    link.addEventListener('click', (e)=>{
+        e.preventDefault()
+        const field = link.dataset.field
+        const value = link.textContent
+        console.log(field, value)
+        filterCards(field, value)
+    })
+})
