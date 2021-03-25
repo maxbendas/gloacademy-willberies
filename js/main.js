cd74ba8a132e69dc9b41bfdb7809a76efef0e5b0
maxbendas <maxbendas86@gmail.com>
2021-03-25 16:10:13 +0200

    Third day

--- js/main.js
+++ js/main.js
@@ -12,8 +12,127 @@ const mySwiper = new Swiper('.swiper-container', {
 
 const buttonCart = document.querySelector('.button-cart')
 const modalCart = document.querySelector('#modal-cart')
+const more = document.querySelector('.more')
+const navigationLinks = document.querySelectorAll('.navigation-link')
+const longGoodsList = document.querySelector('.long-goods-list')
+const btnAccessories = document.querySelector('.accessories')
+const btnClothing = document.querySelector('.clothing')
+const cartTableGoods = document.querySelector('.cart-table__goods')
+const cardTableTotal = document.querySelector('.card-table__total')
+
+const getGoods = async () => {
+    const result = await fetch('db/db.json')
+    if (!result.ok) {
+        throw 'Ошибочка вышла ' + result.status
+    }
+    return await result.json()
+}
+const cart = {
+    cartGoods:
+        [
+            {
+                id: '5',
+                name: "fak",
+                price: 9,
+                count: 3
+            },
+            {
+                id: '6',
+                name: "faaak",
+                price: 19,
+                count: 5
+            }
+        ],
+    renderCart() {
+        cartTableGoods.textContent = ''
+        this.cartGoods.forEach(({id, name, price, count}) => {
+            const trGood = document.createElement('tr')
+            trGood.className = 'cart-item'
+            trGood.dataset.id = id
+            trGood.innerHTML = `
+                <td>${name}</td>
+                <td>${price}$</td>
+                <td><button class="cart-btn-minus">-</button></td>
+                <td>${count}</td>
+                <td><button class="cart-btn-plus">+</button></td>
+                <td>${price * count}$</td>
+                <td><button class="cart-btn-delete">x</button></td>
+            `
+            cartTableGoods.append(trGood)
+        })
+        const totalPrice = this.cartGoods.reduce((sum, item) => {
+            return sum + item.price * item.count
+        }, 0)
+        cardTableTotal.textContent = totalPrice + '$'
+    },
+    deleteGood(id) {
+        this.cartGoods = this.cartGoods.filter(item => id !== item.id)
+        this.renderCart()
+    },
+    plusGood(id) {
+        for (const item of this.cartGoods) {
+            if (item.id === id) {
+                item.count++
+                break
+            }
+        }
+        this.renderCart()
+    },
+    minusGood(id) {
+        for (const item of this.cartGoods) {
+            if (item.id === id) {
+                if (item.count <= 1) {
+                    this.deleteGood(id)
+                } else {
+                    item.count--
+                }
+                break
+            }
+        }
+        this.renderCart()
+    },
+    addCartGoods(id){
+        const goodItem = this.cartGoods.find(item=>item.id===id)
+        if (goodItem){
+            this.plusGood(id)
+        }else{
+            getGoods()
+                .then(data=>data.find(item=>item.id===id))
+                .then(({id, name, price})=>{
+                    this.cartGoods.push({
+                        id,
+                        name,
+                        price,
+                        count:1
+                    })
+                })
+        }
+    }
+}
+
+document.body.addEventListener('click', e=>{
+    const addToCart = e.target.closest('.add-to-cart')
+    if(addToCart){
+        cart.addCartGoods(addToCart.dataset.id)
+    }
+})
+
+cartTableGoods.addEventListener('click', e => {
+    const target = e.target
+    const id = target.closest('.cart-item').dataset.id
+    if (target.classList.contains('cart-btn-delete')) {
+        cart.deleteGood(id)
+    }
+    if (target.classList.contains('cart-btn-minus')) {
+        cart.minusGood(id)
+    }
+    if (target.classList.contains('cart-btn-plus')) {
+        cart.plusGood(id)
+    }
+})
 
 const openModal = () => {
+    cart.renderCart()
     modalCart.classList.add('show')
 }
 
@@ -49,18 +168,6 @@ modalCart.addEventListener('click', (e) => {
 
 // goods
 
-const more = document.querySelector('.more')
-const navigationLinks = document.querySelectorAll('.navigation-link')
-const longGoodsList = document.querySelector('.long-goods-list')
-
-const getGoods = async () => {
-    const result = await fetch('db/db.json')
-    if (!result.ok) {
-        throw 'Ошибочка вышла ' + result.status
-    }
-    return await result.json()
-}
-
 const createCard = ({label, img, name, description, id, price}) => {
     const card = document.createElement('div')
     card.className = 'col-lg-3 col-sm-6'
@@ -94,37 +201,29 @@ more.addEventListener('click', (e) => {
 
 const filterCards = (field, value) => {
     getGoods()
-        .then((data) => {
-            const filteredGoods = data.filter((good) => {
-                return good[field] === value
-            })
-            return filteredGoods
-        })
+        .then(data => data.filter(good => good[field] === value))
         .then(renderCards)
 }
 
-navigationLinks.forEach((link)=>{
-    link.addEventListener('click', (e)=>{
+navigationLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
         e.preventDefault()
         const field = link.dataset.field
         const value = link.textContent
-        if (value === 'All'){
+        if (value === 'All') {
             getGoods().then(renderCards)
-        }else{
+        } else {
             filterCards(field, value)
         }
     })
 })
 
-const btnAccessories = document.querySelector('.accessories')
-const btnClothing = document.querySelector('.clothing')
-
-btnAccessories.addEventListener('click', (e)=>{
+btnAccessories.addEventListener('click', (e) => {
     e.preventDefault()
     filterCards('category', 'Accessories')
 })
 
-btnClothing.addEventListener('click', (e)=>{
+btnClothing.addEventListener('click', (e) => {
     e.preventDefault()
     filterCards('category', 'Clothing')
 })
